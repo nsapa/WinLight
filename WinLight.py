dfdd8d8e5d6a2bef09f5f816dc2d39295f53c82b
Nicolas SAPA <nico@ByMe.at>
2021-03-06 00:32:54 +0100

    Update the python code with additional factures
    - Colored messages
    - Iterate every know URL
    - Check if the image was already downloaded

--- WinLight.py
+++ WinLight.py
@@ -1,57 +1,96 @@
 #! /usr/bin/python
 
 # Import modules
-import fileinput
 import requests
 import re
 import os
+import hashlib
+import pathlib
+import colorama
 
 # Set environment variables
-ImageUrl="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData"
-JsonFile="cache.json"
+ImageUrl = "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData"
 
-#JsonUrl="https://arc.msn.com/v3/Delivery/Placement?&fmt=json&cdm=1&ctry=US&pid=209567"
-#JsonUrl="https://arc.msn.com/v3/Delivery/Placement?&fmt=json&cdm=1&ctry=US&pid=279978"
-#JsonUrl="https://arc.msn.com/v3/Delivery/Placement?&fmt=json&cdm=1&ctry=US&pid=338380"
-JsonUrl="https://arc.msn.com/v3/Delivery/Placement?&fmt=json&cdm=1&ctry=US&pid=338387"
+JsonUrls = [
+    "https://arc.msn.com/v3/Delivery/Placement?&fmt=json&cdm=1&ctry=FR&pid=209567",
+    "https://arc.msn.com/v3/Delivery/Placement?&fmt=json&cdm=1&ctry=FR&pid=279978",
+    "https://arc.msn.com/v3/Delivery/Placement?&fmt=json&cdm=1&ctry=FR&pid=338380",
+    "https://arc.msn.com/v3/Delivery/Placement?&fmt=json&cdm=1&ctry=FR&pid=338387",
+    "https://arc.msn.com/v3/Delivery/Placement?&fmt=json&cdm=1&ctry=FR&pid=338387"
+]
 
-SearchPattern="(?<=imageFileData/).*?(?=\?ver)"
-HttpHeader= {
+SearchPattern = "(?<=imageFileData/).*?(?=\?ver)"
+HttpHeader = {
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'en-US,en;q=0.9',
     'Cache-Control': 'no-cache',
     'User-Agent': 'WindowsShellClient/9.0.40929.0 (Windows)'
 }
 
-# Download JSON file
-Respose = requests.get(JsonUrl, headers=HttpHeader)
-with open(JsonFile,'wb') as file:
-    file.write(Respose.content)
+# Some colors
+ColorRed = colorama.Fore.RED
+ColorGreen = colorama.Fore.GREEN
+ColorBlue = colorama.Fore.BLUE
+ColorCyan = colorama.Fore.CYAN
+ColorYellow = colorama.Fore.YELLOW
+ColorBlack = colorama.Fore.BLACK
+ColorWhite = colorama.Fore.WHITE
+StyleBright = colorama.Style.BRIGHT
+StyleDim = colorama.Style.DIM
+StyleNormal = colorama.Style.NORMAL
+ResetColor = colorama.Style.RESET_ALL
 
-# Remove back slashes
-for line in fileinput.input(JsonFile, inplace=True):
-    print(line.replace("\\", "")),
+# Try all Urls
+for JsonUrl in JsonUrls:
 
-# Remove opening quotes
-for line in fileinput.input(JsonFile, inplace=True):
-    print(line.replace("\"{", "{")),
+    print(f'{ColorWhite}' + 'Parsing ' + f'{StyleDim}' + JsonUrl +
+          f'{StyleNormal}' + ' to find images...' + f'{ResetColor}')
+    # Download JSON file
+    Respose = requests.get(JsonUrl, headers=HttpHeader)
+    content_raw = Respose.text
 
-# Remove closing quotes
-for line in fileinput.input(JsonFile, inplace=True):
-    print(line.replace("}\"", "}")),
+    # Remove back slashes
+    content_raw = content_raw.replace("\\", "")
 
-with open(JsonFile, 'r') as file:
-    content = file.read()
+    # Remove opening quotes
+    content_raw = content_raw.replace("\"{", "{")
 
-# Get the hashes from links and download
-hash = re.findall(SearchPattern, content, re.DOTALL)
-for i in hash:
-    j = ImageUrl + "/" + i
-    FileName = i + ".jpg"
-    print(j)
-    Respose = requests.get(j, headers=HttpHeader)
-    with open(FileName,'wb') as file:
-        file.write(Respose.content)
-    # Delete files less that 2 KB which are blank
-    if os.path.getsize(FileName) < 2048:
-        os.remove(FileName)
+    # Remove closing quotes
+    content_raw = content_raw.replace("}\"", "}")
+
+    # Get the hashes from links and download
+    hash = re.findall(SearchPattern, content_raw, re.DOTALL)
+    for i in hash:
+        j = ImageUrl + "/" + i
+        FileName = i + ".jpg"
+        print(f'{ColorCyan}' + "Downloading " + f'{StyleDim}' + j +
+              f'{ResetColor}')
+        Respose = requests.get(j, headers=HttpHeader)
+        # Ignore files less that 2 KB which are blank
+        if int(Respose.headers['Content-Length']) < 2048:
+            print(f'{ColorRed}' + 'Ignoring ' + i + ': ' + f'{StyleBright}' +
+                  'file too small (' + Respose.headers['Content-Length'] +
+                  ' bytes)' + f'{ResetColor}')
+            continue
+        # Check if the file already exist
+        if os.path.isfile(FileName):
+            OldHash = hashlib.md5(
+                pathlib.Path(FileName).read_bytes()).hexdigest()
+            NewHash = hashlib.md5(Respose.content).hexdigest()
+            # Do we have the same content on disk?
+            if OldHash != NewHash:
+                NewFileName = i + '-' + NewHash + '.jpg'
+                print(f'{ColorCyan}' +
+                      'File already exist with different content, renaming ' +
+                      f'{StyleBright}' + FileName + f'{StyleNormal}' + ' to ' +
+                      f'{StyleBright}' + NewFileName + f'{ResetColor}')
+                FileName = NewFileName
+            print(f'{ColorYellow}' + 'Ignoring ' + i + ': ' + 'We ' +
+                  f'{StyleBright}' + 'already downloaded' + f'{StyleNormal}' +
+                  ' this file with the ' + f'{StyleBright}' +
+                  'exact same content.' + f'{ResetColor}')
+            continue
+        with open(FileName, 'wb') as file:
+            file.write(Respose.content)
+            print(f'{ColorGreen}' + 'Saved as ' + f'{StyleBright}' + FileName +
+                  f'{ResetColor}')
